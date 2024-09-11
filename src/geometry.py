56079c9cf639cdab37cac50f9993bb0ce1bd620c
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-09-12 00:52:38 +0300

    Created classes triangle and box
    Created classes which can be used for easier defining later on

--- src/geometry.py
+++ src/geometry.py
@@ -3,3 +3,18 @@ import pygame.gfxdraw
 from math import sqrt, acos, sin
 
 
+
+
+class Triangle:
+    def __init__(self, a, b, c):
+        self.pointA = a
+        self.pointB = b
+        self.pointC = c
+        self.edges = [[a,b],[a,c],[b,c]]
+
+
+
+
+class Box:
+    def __init__(self):
+        pass
