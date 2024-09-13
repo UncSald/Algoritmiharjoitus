ead77ccce5d131de16949f0d498e91af74970d96
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-09-13 16:27:10 +0300

    Defined some basic class variables needed for later calculations.

--- src/geometry.py
+++ src/geometry.py
@@ -6,11 +6,17 @@ from math import sqrt, acos, sin
 
 
 class Triangle:
-    def __init__(self, a, b, c):
-        self.pointA = a
-        self.pointB = b
-        self.pointC = c
-        self.edges = [[a,b],[a,c],[b,c]]
+    def __init__(self, a_edge, b_edge, c_edge):
+        self._a_edge = a_edge
+        self._b_edge = b_edge
+        self._c_edge = c_edge
+        self._a_point = a_edge[0] 
+        self._b_point = a_edge[1]
+        self._c_point = b_edge[1]
+        self._edges = [a_edge
+                      ,b_edge,
+                      c_edge]
+        self._poins = [a_edge[0], a_edge[1], b_edge[1]]
 
 
 
