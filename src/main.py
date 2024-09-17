c00e8b652cd0d1ab95d8405c9f74681324fc9bf8
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-09-17 17:16:18 +0300

    Created main.py
    main.py will bring all the modules together to create the final program.

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import pygame
+import pygame.gfxdraw
+from geometry import Triangle, Rectangle
+from bowyerWatson import BowyerWatson
+from roomGeneration import generate_rooms
+from listMatrix import list_to_matrix
+from kruskal import kruskal
+
+
+if __name__ == "__main__":
+    pass
