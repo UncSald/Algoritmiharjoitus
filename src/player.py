7214f7a6df19faf3c545f4d7612876f432fb8e04
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-09-28 18:41:05 +0300

    Defined player class.

--- src/player.py
+++ src/player.py
@@ -1,5 +1,19 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, width :int, height :int,\
+                 render_point :tuple[int,int], walls :pygame.sprite.Group):
         pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([width,height])
+        self.image.fill('pink')
+        self.rect = self.image.get_rect(topleft=render_point)
+        self.x, self.y = render_point
+        self.x_velocity = width/8
+        self.y_velocity = height/8
+        self.walls = walls
+
+    def collision(self, group :pygame.sprite.Group):
+        pass
+
+    def update(self):
+        pass
