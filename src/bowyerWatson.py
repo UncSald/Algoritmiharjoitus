ddeab741b8cf0c8d129153b846c0dd4cc67398e0
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-09-28 19:24:15 +0300

    Added comments and functionality.
    If the point added is already in the edge it should be combined with it will not be added to triangulation.

--- src/bowyerWatson.py
+++ src/bowyerWatson.py
@@ -2,21 +2,31 @@ from geometry import Triangle
 
 
 
+# MAIN ALGORITHM FOR THE PROJECT
 
 class BowyerWatson:
+    # DEFINE POINTS, SET CONTAINING FINAL TRIANGULATION
+    # AND EDGES USED IN TRIANGULATION
+    # ALSO ADD SUPER TRIANGLE, WHICH IS ALWAYS BIG ENOUGH TO
+    # CONTAIN THE WHOLE SCREEN WE DRAW ON
     def __init__(self, points, width, height):
         self._points = points
         self._triangulation = set()
         self._all_edges = set()
-        self._original_triangle = Triangle(((-width,height),(width/2,-height)),((-width,height),(2*width,height)),((width/2,-height),(2*width,height)))
+        self._original_triangle = Triangle((-width*2,height*2),\
+                                           (width/2,-height*2), (width*4,height*2))
         self._triangulation.add(self._original_triangle)
 
+    # RUN METHOD TO MAKE SURE EVERYTHING HAPPENS IN THE RIGHT ORDER
     def run(self):
         self.triangulate()
         self.remove_original()
         self.define_edges()
         return self._triangulation
-    
+
+    # TRIANGULATE METHOD CREATES A CORRECT DELAUNAY TRIANGULATION
+    # FOR THE SET OF POINTS BY ADDING A POINT TO THE TRIANGULATION
+    # ONE BY ONE
     def triangulate(self):
         for point in self._points:
             
@@ -25,7 +35,6 @@ class BowyerWatson:
                 if triangle.check_point(point):
                     unusable_triangles.add(triangle)
             usable_edges = set()
-
 
             for triangle in unusable_triangles:
                 for edge in triangle._edges:
@@ -36,15 +45,18 @@ class BowyerWatson:
                     if appears_in_others is False:
                         usable_edges.add(edge)
 
+
             for triangle in unusable_triangles:
                 self._triangulation.remove(triangle)
+            
             for edge in usable_edges:
-                try:
-                    new_triangle = Triangle(edge, (edge[0],point), (edge[1],point))
+                if point not in edge:
+                    new_triangle = Triangle(edge[0], edge[1], point)
                     self._triangulation.add(new_triangle)
-                except ZeroDivisionError:
-                    print("problem with edges: ",edge, (edge[0],point), (edge[1],point))
 
+
+    # REMOVES ALL TRIANGLES CONTAINING POINTS WHICH
+    # BELONG TO THE SUPER TRIANGLE
     def remove_original(self):
         point_with_original = set()
         for point in self._original_triangle._points:
@@ -54,7 +66,11 @@ class BowyerWatson:
         for triangle in point_with_original:
             self._triangulation.remove(triangle)
 
+
+
+    # SIMPLE METHOD TO SAVE ALL THE EDGES IN THE TRIANGULATION
     def define_edges(self):
         for triangle in self._triangulation:
             for edge in triangle._edges:
                 self._all_edges.add(edge)
+                
