d014aa76e72399d1260d77b08388644248c77ca6
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-09-28 19:15:09 +0300

    Create objects has been defined.
    Create_objects takes into account different inputs in the matrix and defines an object accordingly. Floor, wall, or door.

--- src/create_objects.py
+++ src/create_objects.py
@@ -4,4 +4,26 @@ from sprites import Floor, Wall, Door
 
 
 def create_objects(matrix, floor, walls, tile_size):
-    pass
+    for row in range(len(matrix)):
+        for column in range(len(matrix[0])):
+
+            if matrix[row][column] == 1:
+                new_tile = Floor(tile_size, tile_size,\
+                                 (column*tile_size, row*tile_size))
+                new_tile.add(floor)
+            elif matrix[row][column] == 2:
+                new_tile = Floor(tile_size, tile_size,\
+                                 (column*tile_size, row*tile_size))
+                new_tile.add(floor)
+            elif matrix[row][column] == 3:
+                new_tile = Door(tile_size, tile_size,\
+                                 (column*tile_size, row*tile_size))
+                new_tile.add(floor)
+            elif matrix[row][column] == 4:
+                new_tile = Door(tile_size, tile_size,\
+                                 (column*tile_size, row*tile_size))
+                new_tile.add(floor)
+            else:
+                new_tile = Wall(tile_size, tile_size,\
+                                (column*tile_size, row*tile_size))
+                new_tile.add(walls)
