e8bf0695bbc8dee8b40a63933d82c15e4009abf4
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-09-28 18:53:50 +0300

    Created Door class.
    Door class functions for the time being like the floor class, but it will have a different function later.

--- src/sprites.py
+++ src/sprites.py
@@ -1,7 +1,6 @@
 import pygame
 
 
-
 class Wall(pygame.sprite.Sprite):
     def __init__(self, width :int, height :int,\
                  render_point :tuple[int,int]):
@@ -26,6 +25,23 @@ class Floor(pygame.sprite.Sprite):
         self.image = pygame.Surface([width,height])
         self.image.fill('brown')
         self.rect = self.image.get_rect()
+        self.x, self.y = render_point
+    
+    def update(self, player):
+        self.x = self.x - player.changes_x
+        self.y = self.y - player.changes_y
+        self.rect.topleft=(self.x,self.y)
+
+
+
+
+class Door(pygame.sprite.Sprite):
+    def __init__(self, width :int, height :int,\
+                 render_point :tuple[int,int]):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([width,height])
+        self.image.fill('black')
+        self.rect = self.image.get_rect(topleft=render_point)
         self.x, self.y = render_point
 
     def update(self, player):
