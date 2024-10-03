8917168a54b93d6f4922a18711557081b4e8c646
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-03 13:02:41 +0300

    Created two tests testing triangle circumcenter position.

--- src/tests/test_triangle.py
+++ src/tests/test_triangle.py
@@ -9,6 +9,18 @@ class TestTriangle(unittest.TestCase):
         expected = [((100,100), (101,101)), ((100,100), (100,101)), ((101,101), (100,101))]
         self.assertEqual(expected, result)
     
+    def test_triangle_circumcenter1(self):
+        triangle = Triangle((100,100), (101,101), (100,101))
+        result = triangle.circumcenter
+        expected = (100.5, 100.5)
+        self.assertEqual(expected, result)
+    
+    def test_triangle_circumcenter2(self):
+        triangle = Triangle((500,120), (11,300), (123,432))
+        x, y = triangle.circumcenter
+        result = (round(x,3),round(y,3))
+        expected = (254.395, 206.998)
+        self.assertEqual(expected, result)
 
 if __name__ == "__main__":
     unittest.main()
