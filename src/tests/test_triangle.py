88e948a0f6964bd780f6b9932981dc0fc4c84e61
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-03 13:01:37 +0300

    Created first test to check triangle creates edges from points correctly.

--- src/tests/test_triangle.py
+++ src/tests/test_triangle.py
@@ -1,2 +1,14 @@
 import unittest
 from geometry import Triangle
+
+class TestTriangle(unittest.TestCase):
+
+    def test_create_triangle1(self):
+        triangle = Triangle((100,100), (101,101), (100,101))
+        result = triangle._edges
+        expected = [((100,100), (101,101)), ((100,100), (100,101)), ((101,101), (100,101))]
+        self.assertEqual(expected, result)
+    
+
+if __name__ == "__main__":
+    unittest.main()
