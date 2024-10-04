6e5608e30af1bb8560e1fe0f1fe868bf7f3f98e8
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-04 16:30:19 +0300

    test_rectangle tests will check rectangle creation correctness, center correctness, and collisions.

--- src/tests/test_rectangle.py
+++ src/tests/test_rectangle.py
@@ -1,4 +1,28 @@
 import unittest
 from geometry import Rectangle
 
-class TestRectangle(unittest.TestCase)
+class TestRectangle(unittest.TestCase):
+
+    def test_rectangle_creation1(self):
+        pass
+
+    def test_rectangle_creation2(self):
+        pass
+
+    def test_rectangle_center1(self):
+        pass
+
+    def test_rectangle_center2(self):
+        pass
+
+    def test_rectangle_collisions1(self):
+        pass
+
+    def test_rectangle_collisions2(self):
+        pass
+
+    def test_rectangle_collisions3(self):
+        pass
+
+    def test_rectangle_collisions4(self):
+        pass
