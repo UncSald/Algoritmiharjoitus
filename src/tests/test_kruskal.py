e9134eb23c28e1b5b53719c0f7569d41b8d76d40
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-07 13:27:28 +0300

    Created new tests for kruskal.py
    Tests now see if all the points in the given list are included in the final mst.

--- src/tests/test_kruskal.py
+++ src/tests/test_kruskal.py
@@ -35,6 +35,47 @@ class test_kruskal(unittest.TestCase):
         result = len(kruskal(input,(1,1)))
         expected = len(points)-1
         self.assertEqual(expected, result)
+    
+    
+    
+    
+    # TESTS TO CHECK IF ALL POINTS ARE INCLUDED IN THE MST
+    def test_all_points_in_mst1(self):
+        input = set()
+        points = [(i,i) for i in range(10)]
+        result = True
+        for i in points:
+            for j in points:
+                input.add((i,j))
+        for point in points:
+            result = False
+            for edge in input:
+                if point in edge:
+                    result = True
+            if result is False:
+                break
+        expected = True
+        self.assertEqual(expected, result)
+    
+    def test_all_points_in_mst2(self):
+        input = set()
+        points = [(i,i) for i in range(100)]
+        result = True
+        for i in points:
+            for j in points:
+                input.add((i,j))
+        for point in points:
+            result = False
+            for edge in input:
+                if point in edge:
+                    result = True
+            if result is False:
+                break
+        expected = True
+        self.assertEqual(expected, result)
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
