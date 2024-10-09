ce3cb43e712e985f20265405e75ed5a9d29e1536
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-09 11:41:18 +0300

    Updated a_star functions.
    Added weights to edges in new_edge function. Readjusted the functions weighted_graph() and a_star() to properly use this information.

--- src/a_star.py
+++ src/a_star.py
@@ -15,8 +15,7 @@ def build_path(matrix, edges, tile_size:int):
         path = shortest_path(a_star(graph, start, end), start, end)
         for point in path:
             row,col = point
-            if matrix[row][col] == 0:
-                matrix[row][col] = 2
+            matrix[row][col] = 2
     return matrix
 
 
@@ -39,7 +38,7 @@ def shortest_path(previous, start, goal):
 # A* FUNCTION WHICH TAKES IN A MATRIX, A STARTING POINT
 # AND AN ENDPOINT, AND FINDS THE SHORTEST PATH BETWEEN
 # THE POINTS. OUTPUTS TWO DICTS
-def a_star(matrix, start_point, end_point):
+def a_star(graph, start_point, end_point):
     passed = PriorityQueue(0)
     passed.put((start_point, 0))
     previous = {}
@@ -53,8 +52,8 @@ def a_star(matrix, start_point, end_point):
         if location == end_point:
             break
 
-        for next in matrix[location]:
-            cost = cost_so_far[location] + 1
+        for next in graph[location]:
+            cost = cost_so_far[location] + next[1]
             if next[0] not in cost_so_far or cost < cost_so_far[next[0]]:
                 cost_so_far[next[0]] = cost
                 priority = cost + heuristics(next[0], end_point)
@@ -80,47 +79,56 @@ def weighted_graph(matrix):
     nodes = []
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
-            a = (row,column)
-            nodes.append(a)
+            new_node = (row,column)
+            nodes.append(new_node)
     map = {node : [] for node in nodes}
     for node in nodes:
         if node == (0,0):
-            new_edge(node, (0,1), map)
-            new_edge(node, (1,0), map)
+            new_edge(node, (0,1), map,matrix)
+            new_edge(node, (1,0), map,matrix)
         elif node == (len(matrix)-1,len(matrix[0])-1):
-
-            new_edge(node, (len(matrix)-2,len(matrix[0])-1),map)
-            new_edge(node, (len(matrix)-1,len(matrix[0])-2),map)
+            new_edge(node, (len(matrix)-2,len(matrix[0])-1),map,matrix)
+            new_edge(node, (len(matrix)-1,len(matrix[0])-2),map,matrix)
         elif node == (0,len(matrix[0])-1):
-            new_edge(node,(0,len(matrix[0])-2),map)
-            new_edge(node,(1,node[1]),map)
+            new_edge(node,(0,len(matrix[0])-2),map,matrix)
+            new_edge(node,(1,node[1]),map,matrix)
+        elif node == (len(matrix)-1,0):
+            new_edge(node,(len(matrix)-2,0),map,matrix)
+            new_edge(node,(len(matrix)-1,1),map,matrix)
         elif node[0] == 0:
-            new_edge(node,(0,node[1]-1),map)
-            new_edge(node,(1,node[1]),map)
-            new_edge(node, (0,node[1]+1),map)
-
+            new_edge(node,(0,node[1]-1),map,matrix)
+            new_edge(node,(1,node[1]),map,matrix)
+            new_edge(node, (0,node[1]+1),map,matrix)
         elif node[1] == 0:
-            new_edge(node,(node[0]-1,0),map)
-            new_edge(node,(node[0],1),map)
-            new_edge(node, (node[0]+1,0),map)
+            new_edge(node,(node[0]-1,0),map,matrix)
+            new_edge(node,(node[0],1),map,matrix)
+            new_edge(node, (node[0]+1,0),map,matrix)
         elif node[0] == len(matrix)-1:
-            new_edge(node,(len(matrix)-1,node[1]-1),map)
-            new_edge(node,(len(matrix)-2,node[1]),map)
-            new_edge(node, (len(matrix)-1,node[1]+1),map)
+            new_edge(node,(len(matrix)-1,node[1]-1),map,matrix)
+            new_edge(node,(len(matrix)-2,node[1]),map,matrix)
+            new_edge(node,(len(matrix)-1,node[1]+1),map,matrix)
         elif node[1] == len(matrix[0])-1:
-            new_edge(node,(node[0]-1,len(matrix[0])-1),map)
-            new_edge(node,(node[0],len(matrix[0])-2),map)
-            new_edge(node, (node[0]+1,len(matrix[0])-1),map)
+            new_edge(node,(node[0]-1,len(matrix[0])-1),map,matrix)
+            new_edge(node,(node[0],len(matrix[0])-2),map,matrix)
+            new_edge(node, (node[0]+1,len(matrix[0])-1),map,matrix)
         else:
-            new_edge(node,(node[0],node[1]-1),map)
-            new_edge(node,(node[0],node[1]+1),map)
-            new_edge(node,(node[0]-1,node[1]),map)
-            new_edge(node,(node[0]+1,node[1]),map)
+            new_edge(node,(node[0],node[1]-1),map,matrix)
+            new_edge(node,(node[0],node[1]+1),map,matrix)
+            new_edge(node,(node[0]-1,node[1]),map,matrix)
+            new_edge(node,(node[0]+1,node[1]),map,matrix)
     return map
 
 
 
 
 # CREATES A NEW EDGE WITH 1 WHEIGHT TO THE MAP
-def new_edge(node_a, node_b, map):
-    map[node_a].append((node_b, 1))
+def new_edge(node_a, node_b, map, matrix):
+
+    if matrix[node_a[0]-1][node_a[1]-1]==1 or matrix[node_b[0]-1][node_b[1]-1]==1:
+        map[node_a].append((node_b, 2))
+    elif matrix[node_a[0]-1][node_a[1]-1]==9 or matrix[node_b[0]-1][node_b[1]-1]==9:
+        map[node_a].append((node_b, 3))
+    elif matrix[node_a[0]-1][node_a[1]-1]==2 and matrix[node_b[0]-1][node_b[1]-1]==2:
+        map[node_a].append((node_b, 0))
+    else:    
+        map[node_a].append((node_b, 1))
