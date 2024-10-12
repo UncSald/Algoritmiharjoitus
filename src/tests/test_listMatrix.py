e4d149addee10cc360e54b54647a320550cb488f
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-12 20:48:29 +0300

    Created class TestListMatrix.

---
+++ src/tests/test_listMatrix.py
@@ -0,0 +1,8 @@
+import unittest
+from listMatrix import list_to_matrix, point_to_coord, rect_to_coord
+
+class TestListMatrix(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
