7483a870aa3d25d749781723f0ea64c4eb7b0016
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-13 13:22:05 +0300

    Added test for a_star function return value.

--- src/tests/test_a_star.py
+++ src/tests/test_a_star.py
@@ -19,12 +19,20 @@ class TestAStar(unittest.TestCase):
                   [1,2]]
         result = weighted_graph(matrix)
         expected = {
-                    (0,0):[((0,1),5),((1,0),5)],
+                    (0,0):[((0,1),2),((1,0),3)],
                     (0,1):[((0,0),5),((1,1),1)],
                     (1,0):[((0,0),5),((1,1),1)],
-                    (1,1):[((0,1),1),((1,0),1)]
+                    (1,1):[((0,1),2),((1,0),3)]
                     }
         self.assertEqual(expected,result)
+
+    def test_a_star_path(self):
+        graph = weighted_graph([[9,0],
+                                [1,2]])
+        result = a_star(graph,(0,0),(1,1))
+        expected = {(0,0):None, (0,1):(0,0), (1,0):(0,0), (1,1):(0,1)}
+        self.assertEqual(expected,result)
+
 
 if __name__ == "__main__":
     unittest.main()
