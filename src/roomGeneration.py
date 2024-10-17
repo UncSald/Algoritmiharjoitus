0a3d982a026dccec86b0495cad74e6fb4f5111ba
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-17 11:04:47 +0300

    Created bad input handling so the function wont get stuck in a loop.

--- src/roomGeneration.py
+++ src/roomGeneration.py
@@ -1,5 +1,5 @@
 from random import randint
-from geometry import Rectangle
+from src.geometry import Rectangle
 
 # FUNCTION WHICH GENERATES RECTANGLES WHEN GIVEN
 # THE MAX AMOUNT OF RECTANGLES CREATED AND THE SIZE
@@ -11,18 +11,27 @@ def generate_rooms(count :int, width :int, height :int, tile_size :int):
     rect_set = set()
     point_set = set()
 
+
+    if count < 10:
+        if (49*count)/((width/tile_size)*(height/tile_size)) > 0.50:
+            raise ValueError("tilesize too high for this width and height.\
+                              Try increasing screen size or tilesize.")
+    elif (49*count*0.25)/((width/tile_size)*(height/tile_size)) > 0.20:
+        raise ValueError("tilesize too high for this width and height.\
+                          Try increasing screen size or tilesize.")
+
     while len(rect_set) <= count:
         x_point = randint(1, ((width-(width%tile_size))//tile_size))*tile_size
         y_point = randint(1, ((height-(height%tile_size))//tile_size))*tile_size
         max_width = int((width-x_point)//tile_size)-1
         max_height = int((height-y_point)//tile_size)-1
-        if max_width < 3 or max_height < 4:
+        if max_width < 4 or max_height < 4:
             continue
         if max_width > 7:
             max_width = 7
         if max_height > 7:
             max_height = 7
-        rect_width = int(randint(2, max_width)*tile_size)
+        rect_width = int(randint(3, max_width)*tile_size)
         rect_height = int(randint(3, max_height)*tile_size)
         new_rect = Rectangle((x_point,y_point), rect_width, rect_height)
         collision = False
@@ -32,8 +41,11 @@ def generate_rooms(count :int, width :int, height :int, tile_size :int):
         if not collision:
             rect_set.add(new_rect)
             point_set.add(new_rect.center)
-
-    return (rect_set, point_set)
+    avrg = 0
+    for rect in rect_set:
+        avrg += (rect.width/tile_size)*(rect.height/tile_size)
+    avrg = avrg/len(rect_set)
+    return (avrg,rect_set, point_set)
 
 
 
