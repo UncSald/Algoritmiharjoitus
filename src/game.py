9820318ae8181fa44ef1c31cf26ea31591c46567
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-17 20:40:59 +0300

    Added handling for collision with next floor door.
    Also added primal menu and text to be displayed.

--- src/game.py
+++ src/game.py
@@ -11,33 +11,38 @@ from src.create_objects import create_objects
 
 class Game:
     def __init__(self,widht,height,tile):
+        # INIT PYGAME
+        pygame.init()
         self.running = True
         self.widht = widht
         self.height = height
         self.tile_size = tile
+        self.font = pygame.font.SysFont('Arial', 25)
+        
+
         self.final_level = 1
         self.level_num = 0
 
     def run(self):
 
-
-        # INIT PYGAME
-        pygame.init()
-
-
         # ADD BACKGROUND COLOR, FEEL FREE TO CHANGE IT
         BG = pygame.Surface((self.widht,self.height))
-        BG.fill('black')
+        BG.fill((20,49,30))
+
         # DEFINE SCREEN SIZE AND self.height
         # SCREEN SIZE CAN BE CHANGED BUT SHOULD YOU CHANGE IT
         # REMEMBER TO CHANGE THE PLAYER POSITION, AND
         # CHANGES_X/Y ACCORDINGLY
         screen = pygame.display.set_mode((self.widht/2,self.height/2))
+        menu = pygame.surface.Surface((self.widht/2-200,self.height/2-200))
+        menu.fill((200,255,255,0))
+        font = pygame.font.SysFont('Arial', 30, bold=True)
         
         # CREATE LEVEL
-        self.level = self.create_level()
+        self.create_level()
         self.level_num += 1
-        
+        self.yes_no = False
+
         # DEFINE CLOCK FOR SMOOTH SAILING
         clock = pygame.time.Clock()
 
@@ -47,33 +52,51 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+            # DEFINE KEYS
+            keys = pygame.key.get_pressed()
             
             # DRAW ALL SPRITES IN GROUPS
-            screen.blit(BG, (0,0))
-            for sprite in self.walls:
-                screen.blit(sprite.image,(sprite.x,sprite.y))
-                sprite.update(self.player1)
-            for sprite in self.floor:
-                screen.blit(sprite.image,(sprite.x, sprite.y))
-                sprite.update(self.player1)
-            for sprite in self.doors:
-                screen.blit(sprite.image,(sprite.x, sprite.y))
-                sprite.update(self.player1)                
-            for sprite in self.player_group:
-                screen.blit(sprite.image,(sprite.x, sprite.y))
-
-            # UPDATE PLAYER
-            self.player1.update()
-            if self.player1.clear:
-                self.level = self.create_level()
-                self.level_num += 1
-
+            if self.yes_no is not True:
+                screen.blit(BG, (0,0))
+                for sprite in self.walls:
+                    screen.blit(sprite.image,(sprite.x,sprite.y))
+                    sprite.update(self.player1)
+                for sprite in self.floor:
+                    screen.blit(sprite.image,(sprite.x, sprite.y))
+                    sprite.update(self.player1)
+                for sprite in self.doors:
+                    screen.blit(sprite.image,(sprite.x, sprite.y))
+                    sprite.update(self.player1)                
+                for sprite in self.player_group:
+                    screen.blit(sprite.image,(sprite.x, sprite.y))
+                    # UPDATE PLAYER
+                if self.player1.clear is not True:
+                    self.player1.update()
+                else:
+                    self.yes_no = True
+                
+            
+            else:
+                if self.level_num == self.final_level:
+                    screen.blit(menu,(100,100))
+                    screen.blit(font.render('YOU WIN !', True, (255, 0, 255)),(200,200))
+                else:
+                    screen.blit(menu,(100,100))
+                    screen.blit(font.render('Press space to continue', True, (190, 0, 0)),(200,200))
+                    
+                    if keys[pygame.K_SPACE]:
+                        self.player1.changes_x = 0
+                        self.player1.changes_y = 0
+                        self.level_num += 1
+                        self.create_level()
+            
+            screen.blit(font.render(f'B{self.level_num}f', True, (250, 0, 100)),(10,10))
             clock.tick(60)
             pygame.display.flip()
 
     def create_level(self):
 
-    
         # GENERATE ROOMS AND TAKE THEIR COORDINATES AND CENTER POINTS
         rooms_gened = generate_rooms(20 ,self.widht, self.height, self.tile_size)
         rooms, centers = rooms_gened
@@ -87,6 +110,8 @@ class Game:
 
         # DEFINE STARTPOINT AND ENDPOINT
         start_point, self.end_point = start_end(centers, rp_alg._all_edges)
+        print(point_to_coord(start_point,self.tile_size))
+        print(point_to_coord((start_point[0]-self.widht/4,start_point[1]-self.height/4),self.tile_size))
 
         # DEFINE GROUPS FOR WALLS, FLOOR, DOOR, AND PLAYER
         self.walls = pygame.sprite.Group()
@@ -102,9 +127,9 @@ class Game:
         self.player1.changes_y = start_point[1]-self.height/4
 
         # CREATE MAP CONTAINING ROOMS, START, AND GOAL
-        temporaty_map = list_to_matrix(rooms,centers,self.widht, self.height,\
+        temporary_map = list_to_matrix(rooms,centers,self.widht, self.height,\
                             start_point, self.end_point, self.tile_size)
-        print(f"map done, {len(temporaty_map[0])}x{len(temporaty_map)} tiles")
+        print(f"map done, {len(temporary_map[0])}x{len(temporary_map)} tiles")
 
         # CREATE MINIMUM SPANNING TREE FROM EDGES IN TRIANGULATION
         mst = kruskal(rp_alg._all_edges, start_point)
@@ -113,8 +138,9 @@ class Game:
         # CREATE FINAL MAP FROM TEMPORARY ONE
         # THIS TIME IT CONTAINS PATHS BETWEEN ROOMS CREATED
         # BY A_STAR
-        MAP = build_path(temporaty_map,mst,self.tile_size)
+        MAP = build_path(temporary_map,mst,self.tile_size)
         print(f"final map done")
 
         create_objects(MAP, self.tile_size, self.floor, self.walls, self.doors)
         print(f"create objects done")
+        self.yes_no = False
