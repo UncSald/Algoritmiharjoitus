499f04d18c073a8a44c1c1d31715fc03af1f3d8a
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-17 11:27:24 +0300

    Added player 'animation'.

--- src/player.py
+++ src/player.py
@@ -4,8 +4,11 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, width :int, height :int,\
                  render_point :tuple[int,int], walls :pygame.sprite.Group):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([width,height])
-        self.image.fill('pink')
+        self.image = pygame.image.load('src/assets/dude.png')
+        self.image_up = pygame.image.load('src/assets/dude.png')
+        self.image_down = pygame.image.load('src/assets/dude_down.png')
+        self.image_left = pygame.image.load('src/assets/dude_left.png')
+        self.image_right = pygame.image.load('src/assets/dude_right.png')
         self.rect = self.image.get_rect(topleft=render_point)
         self.x, self.y = render_point
         self.x_velocity = width/8
@@ -33,10 +36,14 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.changes_x -= self.x_velocity
+            self.image = self.image_left
         elif keys[pygame.K_d]:
             self.changes_x += self.x_velocity
+            self.image = self.image_right
         elif keys[pygame.K_w]:
             self.changes_y -= self.y_velocity
+            self.image = self.image_up
         elif keys[pygame.K_s]:
             self.changes_y += self.y_velocity
+            self.image = self.image_down
         self.rect.topleft=(self.x,self.y)
