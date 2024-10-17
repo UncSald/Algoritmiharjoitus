c4336858f672973f80a8ef7ed2854b66dcaad961
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-17 11:04:01 +0300

    Created tests for testing generate_rooms() bad input handling.

--- src/tests/test_roomGeneration.py
+++ src/tests/test_roomGeneration.py
@@ -2,7 +2,18 @@ import unittest
 from roomGeneration import generate_rooms
 
 class TestRoomGeneration(unittest.TestCase):
-    pass
+    def test_room_generation_error1(self):
+        try:
+            generate_rooms(1,6,6,1)[0]
+            assert False
+        except ValueError:
+            assert True
+    def test_room_generation_error2(self):
+        try:
+            generate_rooms(2,30,30,2)[0]
+            assert True
+        except ValueError:
+            assert False
 
 if __name__ == "__main__":
     unittest.main()
