2b0fc50cd8238f8836d54b53c79db22d04eb1dd7
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-17 17:45:58 +0300

    Create_objects now takes in a door group and adds goalpoint to the doorgroup. This makes it easier to detect collisions.

--- src/create_objects.py
+++ src/create_objects.py
@@ -4,7 +4,7 @@ from src.sprites import Floor, Wall, Door
 
 # FUNCTION CREATES OBJECTS AND ADDS THEM TO CORRECT GROUPS
 # SO THEY WILL BE DRAWN CORRECTLY
-def create_objects(matrix :list, floor, walls, tile_size :int):
+def create_objects(matrix :list, tile_size :int, floor, walls, doors):
     for row in range(len(matrix)):
         for column in range(len(matrix[0])):
 
@@ -23,7 +23,7 @@ def create_objects(matrix :list, floor, walls, tile_size :int):
             elif matrix[row][column] == 4:
                 new_tile = Door(tile_size, tile_size,\
                                  (column*tile_size, row*tile_size),4)
-                new_tile.add(floor)
+                new_tile.add(doors)
             else:
                 new_tile = Wall(tile_size, tile_size,\
                                 (column*tile_size, row*tile_size))
