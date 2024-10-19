a6ba17f2edccb80fd3166df10affa6a92e482aee
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-19 22:29:42 +0300

    Removed unused args from the constructor.

--- src/player.py
+++ src/player.py
@@ -2,14 +2,12 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, width :int, height :int,\
-                 render_point :tuple[int,int], walls :pygame.sprite.Group, goal :pygame.sprite.Sprite):
+                 render_point :tuple[int,int]):
         """Class constructor generates a new player.
 
         Args:
             width (int): tilesize of the game
             height (int): tilesize of the game
-            walls (pygame.sprite.Group): _description_
-            goal (pygame.sprite.Sprite): _description_
         """
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('src/assets/dude.png')
@@ -21,8 +19,6 @@ class Player(pygame.sprite.Sprite):
         self.x, self.y = render_point
         self.x_velocity = width/8
         self.y_velocity = height/8
-        self.walls = walls
         self.changes_x = 0
         self.changes_y = 0
-        self.goal = goal
         self.clear = False
