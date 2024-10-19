bb0bddb62941f676fab54d6b2ccdf3ccffffa113
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-19 18:12:00 +0300

    demo function draws the created rooms, triangulation, and circumcircles on the screen. There is a short time between the next stage is shown.

--- src/demo.py
+++ src/demo.py
@@ -5,4 +5,46 @@ from src.bowyerWatson import BowyerWatson
 from src.roomGeneration import generate_rooms, start_end
 
 def demo():
-    pass
+    pygame.init()
+    WIDTH = 1500
+    HEIGHT = 900
+
+
+    rooms1, centers1 = generate_rooms(20,WIDTH,HEIGHT,32)
+    bowyer_watson1 = BowyerWatson(centers1,WIDTH,HEIGHT)
+    bowyer_watson1.run()
+
+
+    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    background = pygame.surface.Surface((WIDTH,HEIGHT))
+    background.fill('black')
+    clock = pygame.time.Clock()
+    timer = 0
+    countdown1 = 5
+    countdown2 = 5
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("bye bye")
+                pygame.quit()
+                sys.exit()
+        screen.blit(background,(0,0))
+
+        for room in rooms1:
+            room.draw(screen,'blue')
+            
+        if countdown1 <= 0:
+            for triangle in bowyer_watson1._triangulation:
+                if countdown2 <= 0:
+                    triangle.draw(screen,'red', 1)
+                else:
+                    triangle.draw(screen,'red',0)
+                countdown2-=1/60
+            countdown1 -= 1/60
+        
+        timer += 1/60
+
+        clock.tick(60)
+        pygame.display.flip()
+        
