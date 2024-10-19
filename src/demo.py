9ad48a775a316b99d0cc30aeefc8c742b032d8d8
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-19 18:15:46 +0300

    Added multiple images to be drawn on the screen. Demo shows what happens with a different amount of points on the same surface.

--- src/demo.py
+++ src/demo.py
@@ -8,11 +8,20 @@ def demo():
     pygame.init()
     WIDTH = 1500
     HEIGHT = 900
+    TILESIZE = 1
 
 
     rooms1, centers1 = generate_rooms(20,WIDTH,HEIGHT,32)
     bowyer_watson1 = BowyerWatson(centers1,WIDTH,HEIGHT)
     bowyer_watson1.run()
+
+    rooms2, centers2 = generate_rooms(200,WIDTH,HEIGHT,TILESIZE)
+    bowyer_watson2 = BowyerWatson(centers2,WIDTH,HEIGHT)
+    bowyer_watson2.run()
+
+    rooms3, centers3 = generate_rooms(2000,WIDTH,HEIGHT,TILESIZE)
+    bowyer_watson3 = BowyerWatson(centers3,WIDTH,HEIGHT)
+    bowyer_watson3.run()
 
 
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -30,19 +39,42 @@ def demo():
                 pygame.quit()
                 sys.exit()
         screen.blit(background,(0,0))
-
-        for room in rooms1:
-            room.draw(screen,'blue')
+        if timer < 15:
+            for room in rooms1:
+                room.draw(screen,'blue')
             
-        if countdown1 <= 0:
-            for triangle in bowyer_watson1._triangulation:
-                if countdown2 <= 0:
-                    triangle.draw(screen,'red', 1)
-                else:
-                    triangle.draw(screen,'red',0)
+            if countdown1 <= 0:
+                for triangle in bowyer_watson1._triangulation:
+                    if countdown2 <= 0:
+                        triangle.draw(screen,'red', 1)
+                    else:
+                        triangle.draw(screen,'red',0)
                 countdown2-=1/60
             countdown1 -= 1/60
         
+        elif timer < 30:
+            if countdown1 <= -5 and timer < 20:
+                countdown1 = 5
+            for room in rooms2:
+                room.draw(screen,'blue')
+            
+            if countdown1 <= 0:
+                for triangle in bowyer_watson2._triangulation:
+                        triangle.draw(screen,'red',0)
+            countdown1 -= 1/60
+
+        else:
+            if countdown1 <= -5 and timer < 35:
+                countdown1 = 5
+            for room in rooms3:
+                room.draw(screen,'blue')
+            if countdown1 <= 0:
+                for triangle in bowyer_watson3._triangulation:
+                        triangle.draw(screen,'red',0)
+            countdown1 -= 1/60
+        
+
+
         timer += 1/60
 
         clock.tick(60)
