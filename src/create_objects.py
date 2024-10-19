46925b47d7977e8e0a62ca54e446b7afea5350ff
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-19 23:22:56 +0300

    Changed each created object args accordingly.

--- src/create_objects.py
+++ src/create_objects.py
@@ -16,23 +16,15 @@ def create_objects(matrix :list[list[int]], tile_size :int, floor, walls, doors)
     for row in range(len(matrix)):
         for column in range(len(matrix[0])):
 
-            if matrix[row][column] == 1:
-                new_tile = Floor(tile_size, tile_size,\
-                                 (column*tile_size, row*tile_size),1)
-                new_tile.add(floor)
-            elif matrix[row][column] == 2:
-                new_tile = Floor(tile_size, tile_size,\
-                                 (column*tile_size, row*tile_size),2)
+            if matrix[row][column] == 1 or matrix[row][column] == 2:
+                new_tile = Floor(tile_size,(column*tile_size, row*tile_size))
                 new_tile.add(floor)
             elif matrix[row][column] == 3:
-                new_tile = Door(tile_size, tile_size,\
-                                 (column*tile_size, row*tile_size),3)
+                new_tile = Door(tile_size,(column*tile_size, row*tile_size),3)
                 new_tile.add(floor)
             elif matrix[row][column] == 4:
-                new_tile = Door(tile_size, tile_size,\
-                                 (column*tile_size, row*tile_size),4)
+                new_tile = Door(tile_size,(column*tile_size, row*tile_size),4)
                 new_tile.add(doors)
             else:
-                new_tile = Wall(tile_size, tile_size,\
-                                (column*tile_size, row*tile_size))
+                new_tile = Wall(tile_size,(column*tile_size, row*tile_size))
                 new_tile.add(walls)
