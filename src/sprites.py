b8262aab14182526da59cb91feb299b77229e6d0
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-19 23:19:42 +0300

    Added docstring to each Class and each method.

--- src/sprites.py
+++ src/sprites.py
@@ -1,15 +1,32 @@
 import pygame
 
 
+
+
 class Wall(pygame.sprite.Sprite):
-    def __init__(self, width :int, height :int,\
+    """
+
+    Args:
+        pygame (sprite.Sprite): Super class
+    """
+    def __init__(self, tile_size:int,\
                  render_point :tuple[int,int]):
+        """Class constructor for Wall sprite.
+
+        Args:
+            tile_size (int): Tile size of the game.
+        """
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('src/assets/wall.png')
         self.rect = self.image.get_rect(topleft=render_point)
         self.x, self.y = render_point
 
     def update(self, player):
+        """Update function to have the objects move around the player.
+
+        Args:
+            player (Player): Player used for the update function.
+        """
         self.x = self.x - player.changes_x
         self.y = self.y - player.changes_y
         self.rect.topleft=(self.x,self.y)
@@ -18,8 +35,18 @@ class Wall(pygame.sprite.Sprite):
 
 
 class Floor(pygame.sprite.Sprite):
-    def __init__(self, width :int, height :int,\
-                 render_point :tuple[int,int], value):
+    """Class constructor for Floor sprite.
+
+    Args:
+        pygame (sprite.Sprite): Super class
+    """
+    def __init__(self, tile_size:int,\
+                 render_point :tuple[int,int]):
+        """Class constructor for Floor sprite.
+
+        Args:
+            tile_size (int): Tile size of the game.
+        """
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load('src/assets/floor_2.png')
@@ -28,6 +55,11 @@ class Floor(pygame.sprite.Sprite):
         self.x, self.y = render_point
     
     def update(self, player):
+        """Update function to have the objects move around the player.
+
+        Args:
+            player (Player): Player used for the update function.
+        """
         self.x = self.x - player.changes_x
         self.y = self.y - player.changes_y
         self.rect.topleft=(self.x,self.y)
@@ -36,8 +68,19 @@ class Floor(pygame.sprite.Sprite):
 
 
 class Door(pygame.sprite.Sprite):
-    def __init__(self, width :int, height :int,\
+    """Class constructor for Door sprite.
+
+    Args:
+        pygame (sprite.Sprite): Super class
+    """
+    def __init__(self, tile_size:int,\
                  render_point :tuple[int,int], value):
+        """Class constructor for Door sprite.
+
+        Args:
+            tile_size (int): Tile size of the game.
+            value (_type_): Defines the image used in rendering. 3 Is the start point, 4 is the goal point.
+        """
         pygame.sprite.Sprite.__init__(self)
         if value == 3:
             self.image = pygame.image.load('src/assets/prev_door.png')
@@ -47,12 +90,32 @@ class Door(pygame.sprite.Sprite):
         self.x, self.y = render_point
 
     def update(self, player):
+        """Update function to have the objects move around the player.
+
+        Args:
+            player (Player): Player used for the update function.
+        """
         self.x = self.x - player.changes_x
         self.y = self.y - player.changes_y
         self.rect.topleft=(self.x,self.y)
 
+
+
+
 class Item(pygame.sprite.Sprite):
+    """Class constructor for Item sprite.
+
+    Args:
+        pygame (sprite.Sprite): Super class
+    """
     def __init__(self,render_point :tuple[int,int], image, name):
+        """Class constructor for Item sprite.
+
+        Args:
+            render_point (tuple[int,int]): Point of origin for the sprite center.
+            image (pygame.image): Image used for item creation.
+            name (str): Name for the item.
+        """
         pygame.sprite.Sprite.__init__(self)
         self.name = name
         self.image = image
@@ -60,6 +123,11 @@ class Item(pygame.sprite.Sprite):
         self.x, self.y = render_point
 
     def update(self, player):
+        """Update function to have the objects move around the player.
+
+        Args:
+            player (Player): Player used for the update function.
+        """
         self.x = self.x - player.changes_x
         self.y = self.y - player.changes_y
         self.rect.topleft=(self.x,self.y)
