3316c3165ab77fb2fad4b58ec067e7bc8f50d3ad
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-22 15:31:28 +0300

    Cleaned up main function and added docstring.

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import src.game
 from src.demo import demo
 
 
-# DEFINE MAIN FUNCTION
 def main():
+    """Main function for the project.
+    Calls the wanted function through user input.
+    """
 
-        decision = str(input('Type "demo" to see demo, "game" to play the game, anything else exits: '))
-        
-        if decision == 'demo':
-            demo()
+    decision = str(input('Type "demo" to see demo, "game" to play the game, anything else exits: '))
 
-        elif decision == 'game':
-            # DEFINE WIDTH, HEIGHT, AND TILE SIZE.
-            # FEEL FREE TO CHANGE THESE AND SEE HOW THE MAP GENERATES
-            WIDTH = 1500
-            HEIGHT = 1000
+    if decision == 'demo':
+        demo()
 
-            new_game = src.game.Game(WIDTH,HEIGHT)
+    elif decision == 'game':
+        width = 1500
+        height = 1000
 
-            new_game.run()
-        else:
-            print("bye bye")
+        new_game = src.game.Game(width,height)
+
+        new_game.run()
+    else:
+        print("bye bye")
 
 
 if __name__ == "__main__":
