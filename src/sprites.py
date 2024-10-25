05d322ffadd2cd64786163c7be5289b1f5a7bbfc
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-25 19:49:02 +0300

    Fused Floor, Wall, and Door classes to one GameTile -class. Class defines drawn sprite by given value in constructor.

--- src/sprites.py
+++ src/sprites.py
@@ -3,24 +3,29 @@ import pygame
 
 
 
-class Wall(pygame.sprite.Sprite):
-    """
-
+class GameTile(pygame.sprite.Sprite):
+    """ GameTile class holds information of game tiles such as,
+    sprite and location.
     Args:
         pygame (sprite.Sprite): Super class
     """
     def __init__(self, tile_size:int,\
                  render_point :tuple[int,int],val):
-        """Class constructor for Wall sprite.
+        """Class constructor for GameTile sprite.
 
         Args:
             tile_size (int): Tile size of the game.
         """
         pygame.sprite.Sprite.__init__(self)
-        if val==5:
+        if val==1 or val==2:
+            self.image = pygame.image.load('src/assets/floor_2.png')
+        elif val==3:
+            self.image = pygame.image.load('src/assets/prev_door.png')
+        elif val==4:
+            self.image = pygame.image.load('src/assets/next_door.png')
+        elif val == 5:
             self.image = pygame.image.load('src/assets/wall.png')
         else:
-            
             self.image = pygame.image.load('src/assets/floor.png')
         self.image = pygame.transform.scale(self.image,(tile_size,tile_size))
         self.rect = self.image.get_rect(topleft=render_point)
@@ -35,79 +40,6 @@ class Wall(pygame.sprite.Sprite):
         self.x = self.x - player.changes_x
         self.y = self.y - player.changes_y
         self.rect.topleft=(self.x,self.y)
-
-
-
-
-class Floor(pygame.sprite.Sprite):
-    """Class constructor for Floor sprite.
-
-    Args:
-        pygame (sprite.Sprite): Super class
-    """
-    def __init__(self, tile_size:int,\
-                 render_point :tuple[int,int]):
-        """Class constructor for Floor sprite.
-
-        Args:
-            tile_size (int): Tile size of the game.
-        """
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = pygame.image.load('src/assets/floor_2.png')
-        self.image = pygame.transform.scale(self.image,(tile_size,tile_size))
-
-        self.rect = self.image.get_rect()
-        self.x, self.y = render_point
-    
-    def update(self, player):
-        """Update function to have the objects move around the player.
-
-        Args:
-            player (Player): Player used for the update function.
-        """
-        self.x = self.x - player.changes_x
-        self.y = self.y - player.changes_y
-        self.rect.topleft=(self.x,self.y)
-
-
-
-
-class Door(pygame.sprite.Sprite):
-    """Class constructor for Door sprite.
-
-    Args:
-        pygame (sprite.Sprite): Super class
-    """
-    def __init__(self, tile_size:int,\
-                 render_point :tuple[int,int], value):
-        """Class constructor for Door sprite.
-
-        Args:
-            tile_size (int): Tile size of the game.
-            value (_type_): Defines the image used in rendering. 3 Is the start point, 4 is the goal point.
-        """
-        pygame.sprite.Sprite.__init__(self)
-        if value == 3:
-            self.image = pygame.image.load('src/assets/prev_door.png')
-        if value == 4:
-            self.image = pygame.image.load('src/assets/next_door.png')
-        self.image = pygame.transform.scale(self.image,(tile_size,tile_size))
-        self.rect = self.image.get_rect(topleft=render_point)
-        self.x, self.y = render_point
-
-    def update(self, player):
-        """Update function to have the objects move around the player.
-
-        Args:
-            player (Player): Player used for the update function.
-        """
-        self.x = self.x - player.changes_x
-        self.y = self.y - player.changes_y
-        self.rect.topleft=(self.x,self.y)
-
-
-
 
 class Item(pygame.sprite.Sprite):
     """Class constructor for Item sprite.
