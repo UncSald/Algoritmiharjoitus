f639706db4589c510f70b81fcbbd9face1bdb920
Alingue Marc I J <abmarc@cubbli24-gold.cs.helsinki.fi>
2024-10-27 19:30:37 +0200

    Changed demo function to Demo class.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import src.game
-from src.demo import demo
+from src.demo import Demo
 
+"""Contains the main function for the project.
+"""
 
 def main():
     """Main function for the project.
@@ -10,13 +12,13 @@ def main():
     decision = str(input('Type "demo" to see demo, "game" to play the game, anything else exits: '))
 
     if decision == 'demo':
-        demo()
+        demo = Demo(1500,900,1)
+        demo.run()
 
     elif decision == 'game':
-
         new_game = src.game.Game()
-
         new_game.run()
+
     else:
         print("bye bye")
 
